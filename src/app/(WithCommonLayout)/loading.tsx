4e860b72067bbadb879e433f1327cbcf788f9ec2
hmmasudsarder <hmmasudsardar@gmail.com>
2025-03-03 13:49:22 +0600

    fixed loading bug

--- src/app/(WithCommonLayout)/loading.tsx
+++ src/app/(WithCommonLayout)/loading.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const loading = () => {
   return (
-    <div>
-      Loading
+    <div className=' h-screen flex items-center justify-center'>
+      <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-[#40282C]"></div>
     </div>
-  )
-}
+  );
+};
 
-export default loading
+export default loading;
