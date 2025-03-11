b6c31ca00b85f4f4318251629c9d6821e047dbef
hmmasudsarder <hmmasudsardar@gmail.com>
2025-03-11 21:02:06 +0600

    added profile components or page

---
+++ src/app/(WithDashboardLayout)/student/myrequest/page.tsx
@@ -0,0 +1,12 @@
+import AcceptRequest from '@/components/dashboard/Student/AcceptRequest'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <AcceptRequest/>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/(WithDashboardLayout)/student/profile/page.tsx
@@ -0,0 +1,12 @@
+import Profile from '@/components/dashboard/profile/profile'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Profile/>
+    </div>
+  )
+}
+
+export default page
