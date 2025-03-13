aa8b693dd55f853dbd3fea48ad79902edb0b43af
hmmasudsarder <hmmasudsardar@gmail.com>
2025-03-13 15:52:06 +0600

    change url

--- middleware.ts
+++ middleware.ts
@@ -23,7 +23,7 @@ export const middleware = async (request: NextRequest) => {
         } else {
             return NextResponse.redirect(
                 new URL(
-                    `http://localhost:3000/login?redirectPath=${pathname}`,
+                    `https://tutor-link-client.vercel.app/login?redirectPath=${pathname}`,
                     request.url
                 )
             );
